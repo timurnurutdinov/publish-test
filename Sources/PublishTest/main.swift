653cdbd32c1ac685c4534792f3e631c27a815841
Timur Nurutdinov <addleimb@gmail.com>
2020-12-20 23:15:23 +0300

    test2

--- Sources/PublishTest/main.swift
+++ Sources/PublishTest/main.swift
@@ -3,11 +3,12 @@
 import Foundation
 import Publish
 import Plot
+import Files
 
 // This type acts as the configuration for your website.
 struct PublishTest: Website {
     enum SectionID: String, WebsiteSectionID {
-        case posts
+        case prototypes
     }
 
     struct ItemMetadata: WebsiteItemMetadata {
@@ -15,9 +16,9 @@ struct PublishTest: Website {
     }
 
     // Update these properties to configure your website:
-    var url = URL(string: "https://your-website-url.com")!
-    var name = "Is it for me?"
-    var description = "A description of PublishTest"
+    var url = URL(string: "https://tilllur.ru")!
+    var name = "TillluR"
+    var description = "Work in Progress"
     var language: Language { .english }
     var imagePath: Path? { nil }
 }
@@ -25,9 +26,10 @@ struct PublishTest: Website {
 // This will generate your website using the built-in Foundation theme:
 try PublishTest().publish(using: [
     .addMarkdownFiles(),
-    .copyResources(at: "Content"),
+    .readFiles(),
+//    .copyResources(at: "Content"),
 //    .addFavoriteItems(),
-//    .addDefaultSectionTitles(),
+    .addDefaultSectionTitles(),
     .generateHTML(withTheme: .foundation),
     
     
@@ -35,3 +37,49 @@ try PublishTest().publish(using: [
 //    .deploy(using: .gitHub("    timurnurutdinov/timurnurutdinov.github.io", useSSH: false))
     ]
 )
+
+
+
+extension PublishingStep where Site == PublishTest {
+    
+    static func addDefaultSectionTitles() -> Self {
+        .step(named: "Default section titles") { context in
+            context.mutateAllSections { section in
+                guard section.title.isEmpty else { return }
+
+                switch section.id {
+                case .prototypes:
+                    section.title = "My Prototypes"
+//                case .links:
+//                    section.title = "External links"
+//                case .about:
+//                    section.title = "About this site"
+                }
+            }
+        }
+    }
+    
+    
+    static func readFiles() -> Self {
+        .step(named: "Read Prototype Folder") { context in
+            
+//            var prototypeNames = [String]()
+//
+//            for file in try Folder(path: "~/Desktop/test/").subfolders {
+//                prototypeNames.append(file.name)
+//                let parsePrototypeInstance = ParsePrototype(name: file.name)
+//                parsePrototypeInstance.printName()
+//            }
+//
+            var prototypes = [ParsePrototype]()
+            let mainFolderPath = "~/Desktop/test/"
+            
+            try Folder(path: mainFolderPath).subfolders.enumerated().forEach { (index, folder) in
+                prototypes.append(ParsePrototype.init(name: folder.nameExcludingExtension))
+            }
+            
+            
+        }
+    }
+    
+}
