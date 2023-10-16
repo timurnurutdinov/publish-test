44eb868b4f00e6873375341e17e20a083f502243
Timur Nurutdinov <addleimb@gmail.com>
2023-10-16 22:14:59 +0300

    updated to ItemJSON?

--- Sources/PublishTest/Classes/Prototype+JSON.swift
+++ Sources/PublishTest/Classes/Prototype+JSON.swift
@@ -12,10 +12,14 @@ import Files
 
 extension Prototype {
     public func saveJSON() { self.json.save(json: self.json, withName: "tilllur.json", toFolder: self.jsonFolder()) }
-    public func readJSON() -> PrototypeJSON { return PrototypeJSON.read(file: self.jsonFile()) }
+    public func readJSON() -> PrototypeJSON? {
+        if let prototypeWithValidJSON = PrototypeJSON.read(file: self.jsonFile()) { return prototypeWithValidJSON }
+        return nil
+    }
 }
 
 public struct PrototypeJSON: Codable, Hashable, ItemJSON {
+    
     public typealias DataType = PrototypeJSON
     
     public var seed: String
@@ -24,7 +28,10 @@ public struct PrototypeJSON: Codable, Hashable, ItemJSON {
     public var star: Bool
     
     init(seed: String, url: String, open: Bool, star: Bool) { self.seed = seed; self.url = url; self.open = open; self.star = star }
-    init(jsonFile: File) { self = PrototypeJSON.read(file: jsonFile) }
+    init(jsonFile: File?) {
+        if let prototypeWithValidJSON = PrototypeJSON.read(file: jsonFile) { self = prototypeWithValidJSON }
+        else { self = PrototypeJSON(seed: "none", url: "", open: false, star: false) }
+    }
 }
 
 
@@ -38,29 +45,41 @@ public struct PrototypeJSON: Codable, Hashable, ItemJSON {
 public protocol ItemJSON {
     associatedtype DataType: Codable
 
-    static func read(file: File?) -> DataType
-    func save(json: DataType, withName jsonName: String, toFolder folder: Folder)
+    static func read(file: File?) -> DataType?
+    func save(json: DataType, withName jsonName: String, toFolder folder: Folder?)
 }
 
 extension ItemJSON {
     
-    public static func read(file: File?) -> DataType {
+    public static func read(file: File?) -> DataType? {
         do {
             
             if let file = file { return try JSONDecoder().decode(DataType.self, from: file.readData())}
-            else { fatalError("File \(String(describing: file)) does't exist") }
+            else {
+                print("File \(String(describing: file)) does't exist")
+                return nil
+            }
             
-        } catch { fatalError("Error decoding JSON: \(error)") }
+        } catch {
+            print("Error decoding JSON:")
+            return nil
+            
+        }
     }
     
-    public func save(json: DataType, withName jsonName: String, toFolder folder: Folder) {
+    public func save(json: DataType, withName jsonName: String, toFolder folder: Folder?) {
         do {
             
             let encoder = JSONEncoder()
             encoder.outputFormatting = .prettyPrinted
             
             let data = try encoder.encode(json)
-            if let jsonString = String(data: data, encoding: .utf8) { jsonString.writeFile(jsonName, toFolder: folder.path) }
+            if let jsonString = String(data: data, encoding: .utf8) {
+                if let unwrappedFolder = folder {
+                    jsonString.writeFile(jsonName, toFolder: unwrappedFolder.path)
+                }
+                
+            }
 
         } catch { print(error) }
     }
@@ -70,21 +89,22 @@ extension ItemJSON {
 
 extension Prototype {
     
-    
-    public func jsonFolder() -> Folder {
+    public func jsonFolder() -> Folder? {
         do { return try Folder(path: folder.path).createSubfolderIfNeeded(withName: "tilllur") }
         catch {
-            fatalError("jsonFolder read failed")
+            print("Failed to read jsonFolder for \(self.name.origin)")
+            return nil
         }
     }
     
-    public func jsonFile(_ fileName: String = "tilllur.json") -> File {
+    public func jsonFile(_ fileName: String = "tilllur.json") -> File? {
         do {
             try Folder(path: folder.path).createSubfolderIfNeeded(withName: "tilllur")
             return try Folder(path: folder.path + "tilllur/").file(at: fileName)
         }
         catch {
-            fatalError("jsonFolder read failed")
+            print("Failed to read jsonFile for \(self.name.origin)")
+            return nil
         }
     }
 }
